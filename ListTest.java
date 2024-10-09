9b47720c5b76d69e1225027bf3e2ba036ab90457
FerVini <111383455+FerVini@users.noreply.github.com>
2024-10-08 21:16:35 -0300

    Add files via upload

---
+++ ListTest.java
@@ -0,0 +1,13 @@
+// Importação de componentes
+import javax.swing.*;
+
+public class ListTest // Declaração de classe
+{ // Abertura do bloco de códigos
+   public static void main(String[] args) // Declaração do método principal
+   { // Abertura do bloco de códigos
+      ListFrame listFrame = new ListFrame(); // Declaração de variável que recebe  um objeto
+      listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Define a operação de fechamento padrão (sair ao fechar)
+      listFrame.setSize(350, 150); // Define o tamanho da janela
+      listFrame.setVisible(true); // Define a visibilidade da tela
+   } // Encerra o bloco de códigos
+} // Encerra o bloco de códigos
