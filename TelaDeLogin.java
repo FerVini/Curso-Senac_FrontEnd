fb5539480b6ec45dde80d83cca1610c09693cbc4
FerVini <111383455+FerVini@users.noreply.github.com>
2024-10-17 21:52:11 -0300

    Add files via upload

--- TelaDeLogin.java
+++ TelaDeLogin.java
@@ -1,15 +1,18 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.sql.*;
 
 public class TelaDeLogin extends JFrame {
     private final JLabel lblLogin;
     private final JLabel lblSenha;
+    private final JLabel lblNotificacoes;
 
     private final JTextField txtLogin;
     private final JPasswordField txtSenha;
 
     private final JButton btnLogar;
+
 
     public TelaDeLogin() {
         super("Tela de Login");
@@ -19,24 +22,46 @@ public class TelaDeLogin extends JFrame {
         lblLogin.setToolTipText("Login");
         add(lblLogin);
 
-        txtLogin = new JTextField(15);
+        txtLogin = new JTextField(10);
         add(txtLogin);
 
         lblSenha = new JLabel("Senha:");
         lblSenha.setToolTipText("Senha");
         add(lblSenha);
 
-        txtSenha = new JPasswordField(15);
+        txtSenha = new JPasswordField(10);
         add(txtSenha);
 
         btnLogar = new JButton("Login");
         add(btnLogar);
+
+        lblNotificacoes = new JLabel("Notificações");
+        add(lblNotificacoes);
+    }
+
+    private class ButtonHandler implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent event){
+            try {
+                Connection conexao = MySQLConnector.conectar();
+                String strSqlLogin = "select * from `db_senac`.`tbl_senac` where `email` = '" + txtLogin.getText() + "' and `senha` = '" + String.valueOf(txtSenha.getPassword()) + "';"; 
+                Statement stmSqlLogin = conexao.createStatement();
+                ResultSet rstSqlLogin = stmSqlLogin.executeQuery(strSqlLogin);
+                if(rstSqlLogin.next()){
+                    // Aqui vamos notificar o usuario que o login e senha foram encontrados
+                } else {
+                    // Aqui vamos notificar o usuario que o login e senha não foram encontrados
+                }
+            } catch(Exception e){
+                System.err.println("Ops! Deu ruim, se liga no erro: " + e);
+            }
+        }
     }
     
     public static void main(String[] args){
         TelaDeLogin appTelaDeLogin = new TelaDeLogin();
         appTelaDeLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        appTelaDeLogin.setSize(200, 250);
+        appTelaDeLogin.setSize(150, 200);
         appTelaDeLogin.setVisible(true);
     }
 }
