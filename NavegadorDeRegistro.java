ad6383a5461e5ba0b2cedd5e396f38918f4e15d9
FerVini <111383455+FerVini@users.noreply.github.com>
2024-10-29 20:30:08 -0300

    Add files via upload

--- NavegadorDeRegistro.java
+++ NavegadorDeRegistro.java
@@ -30,14 +30,67 @@ public class NavegadorDeRegistro extends TelaDePesquisa {
     }
 
     public static void vaParaRegistroAnterior() {
-                
+        try {
+            String strSqlProxRegistro = "select * from `db_senac`.`tbl_senac` where `id` < " + txtId.getText() + " order by `id` desc;";
+            Connection conexao = MySQLConnector.conectar();
+            Statement stmSqlProxRegistro = conexao.createStatement();
+            ResultSet rstSqlProxRegistro = stmSqlProxRegistro.executeQuery(strSqlProxRegistro);
+            if (rstSqlProxRegistro.next()) {
+                txtId.setText(rstSqlProxRegistro.getString("id"));
+                txtNome.setText(rstSqlProxRegistro.getString("nome"));
+                txtEmail.setText(rstSqlProxRegistro.getString("email"));
+                notificarUsuario("Primeiro registro posicionado com sucesso!");
+                habilitarTodos();
+            } else {
+                notificarUsuario("Não foram encontrados registros.");
+            }
+        } catch (Exception e) {
+            notificarUsuario("Ops! Houve um problema no servidor e não será possível inicializar os registros no momento. Por favor, retorne novamente mais tarde.");
+            System.err.println("Erro: " + e);
+        }
     }
 
     public static void vaParaProximoRegistro() {
-        
+        try {
+            String strSqlProxRegistro = "select * from `db_senac`.`tbl_senac` where `id` > " + txtId.getText() +" order by `id` asc;";
+            Connection conexao = MySQLConnector.conectar();
+            Statement stmSqlProxRegistro = conexao.createStatement();
+            ResultSet rstSqlProxRegistro = stmSqlProxRegistro.executeQuery(strSqlProxRegistro);
+            if (rstSqlProxRegistro.next()) {
+                txtId.setText(rstSqlProxRegistro.getString("id"));
+                txtNome.setText(rstSqlProxRegistro.getString("nome"));
+                txtEmail.setText(rstSqlProxRegistro.getString("email"));
+                notificarUsuario("Primeiro registro posicionado com sucesso!");
+                habilitarTodos();
+            } else {
+                notificarUsuario("Não foram encontrados registros.");
+            }
+        } catch (Exception e) {
+            notificarUsuario("Ops! Houve um problema no servidor e não será possível inicializar os registros no momento. Por favor, retorne novamente mais tarde.");
+            System.err.println("Erro: " + e);
+        }
     }
-
+    
     public static void vaParaUltimoRegistro() {
-
+        try {
+            String strSqlInicializacao = "select * from `db_senac`.`tbl_senac` order by `id` desc;";
+            Connection conexao = MySQLConnector.conectar();
+            Statement stmSqlInicializacao = conexao.createStatement();
+            ResultSet rstSqlInicializacao = stmSqlInicializacao.executeQuery(strSqlInicializacao);
+            if (rstSqlInicializacao.next()) {
+                txtId.setText(rstSqlInicializacao.getString("id"));
+                txtNome.setText(rstSqlInicializacao.getString("nome"));
+                txtEmail.setText(rstSqlInicializacao.getString("email"));
+                notificarUsuario("Primeiro registro posicionado com sucesso!");
+                habilitarVoltar();
+            } else {
+                notificarUsuario("Não foram encontrados registros.");
+            }
+        } catch (Exception e) {
+            notificarUsuario("Ops! Houve um problema no servidor e não será possível inicializar os registros no momento. Por favor, retorne novamente mais tarde.");
+            System.err.println("Erro: " + e);
+        }
     }
+
 }
+
